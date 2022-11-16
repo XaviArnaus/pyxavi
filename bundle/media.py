9bcfe657de9eb41aafb0fdf71591045cf4e909ad
Xavi <xavi@arnaus.net>
2022-11-16 10:16:37 +0100

    Adding a Media Class, bumping to 0.0.6

---
+++ bundle/media.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+
+class Media:
+
+    def get_media_url_from_text(self, text: str) -> str:
+        soup = BeautifulSoup(text, 'html.parser')
+        img = soup.select('img')
+        if img:
+            return [{"url": i['src'],"alt_text": i['alt'] if "alt" in i else None} for i in img if i['src']]
+        else:
+            return None
+        
