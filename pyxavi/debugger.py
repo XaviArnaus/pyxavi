5303e2460b9416f0319aad8790caaa57c61c7cd1
Xavier <xavi@arnaus.net>
2023-04-28 07:23:54 +0200

    Add traceback feature in debugger lib

--- pyxavi/debugger.py
+++ pyxavi/debugger.py
@@ -23,3 +23,32 @@ def dd(var, prefix=''):
                 print(prefix, i, ': (', var[i].__class__.__name__, ') ', var[i], sep='')
             else:
                 print(prefix, '(', i.__class__.__name__, ') ', i, sep='')
+
+def full_stack():
+    """
+    `print full_stack()` will print the full stack trace up to the top, 
+    including e.g. IPython's interactiveshell.py calls, 
+    since there is (to my knowledge) no way of knowing who would catch exceptions. 
+    It's probably not worth figuring out anyway...
+
+    If `print full_stack()` is called from within an `except` block,
+    `full_stack` will include the stack trace down to the raise. 
+    In the standard Python interpreter, this will be identical to the message
+    you receive when not catching the exception (Which is why that del stack[-1] is there,
+    you don't care about the except block but about the try block).
+
+    Kindly stolen from:
+    https://stackoverflow.com/questions/6086976/how-to-get-a-complete-exception-stack-trace-in-python
+
+    """
+    import traceback, sys
+    exc = sys.exc_info()[0]
+    stack = traceback.extract_stack()[:-1]  # last one would be full_stack()
+    if exc is not None:  # i.e. an exception is present
+        del stack[-1]       # remove call of full_stack, the printed exception
+                            # will contain the caught exception caller instead
+    trc = 'Traceback (most recent call last):\n'
+    stackstr = trc + ''.join(traceback.format_list(stack))
+    if exc is not None:
+         stackstr += '  ' + traceback.format_exc().lstrip(trc)
+    return stackstr
