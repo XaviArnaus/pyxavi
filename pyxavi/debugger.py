82a8a6b557d7fec3b42cd7135854c6b461fccc00
Xavi <xavi@arnaus.net>
2023-08-03 10:12:16 +0200

    Remove the old function

--- pyxavi/debugger.py
+++ pyxavi/debugger.py
@@ -1,32 +1,15 @@
 from types import NoneType
 
-def dd1(var, prefix=''):
+def dd(what: any, export: bool = False):
     """Function to print the inner of a variable
 
     You know you're a php developer when the first thing you ask for
     when learning a new language is 'Where's var_dump?????'
 
-    Kindly stolen from:
-    https://stackoverflow.com/questions/383944/what-is-a-python-equivalent-of-phps-var-dump
-
     :Authors:
         Xavier Arnaus <xavi@arnaus.net>
 
     """
-
-    my_type = '[' + var.__class__.__name__ + '(' + str(len(var)) + ')]:'
-    print(prefix, my_type, sep='')
-    prefix += '    '
-    for i in var:
-        if type(i) in (list, tuple, dict, set):
-            dd1(i, prefix)
-        else:
-            if isinstance(var, dict):
-                print(prefix, i, ': (', var[i].__class__.__name__, ') ', var[i], sep='')
-            else:
-                print(prefix, '(', i.__class__.__name__, ') ', i, sep='')
-
-def dd(what: any, export: bool = False):
     is_complex, result = dump(what)
     if export:
         return result
