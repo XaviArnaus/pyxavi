757cfe9ac791b8f84529f461a2a8d8c93392b885
Xavi <xavi@arnaus.net>
2023-11-06 07:49:16 +0100

    Add some descriptions in each dictionary method

--- pyxavi/dictionary.py
+++ pyxavi/dictionary.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 PATH_SEPARATOR_CHAR = "."
 
 
@@ -31,11 +33,16 @@ class Dictionary:
             if path_separator_char is not None else PATH_SEPARATOR_CHAR
 
     def _is_int(self, element: str) -> bool:
+        """Check if the given element is an integer without converting it"""
         if element[0] in ('-', '+'):
             return element[1:].isdecimal()
         return element.isdecimal()
 
     def get(self, param_name: str = "", default_value: any = None) -> any:
+        """
+        Returns the value found in the given param_name path,
+        otherwise default_value is returned
+        """
         if param_name.find(self._separator) > 0:
             # bring it local so we can play with it
             local_content = self._content
@@ -70,9 +77,11 @@ class Dictionary:
             else default_value
 
     def get_all(self) -> dict:
+        """Returns the whole internal dictionary"""
         return self._content
 
     def _is_out_of_range(self, index: int, list_to_check: list) -> bool:
+        """Checks if the given index is out of range for the given list"""
         try:
             list_to_check[index]
             return False
@@ -80,7 +89,13 @@ class Dictionary:
             return True
 
     def __recursive_set(self, param_name: str, value: any, dictionary: any = None) -> None:
+        """
+        Recursively walks through the dictionary to find the key to set, and sets it
 
+        Raises a RuntimeError if any of the keys in the param_name does not exist
+        Raises a ValueError if a key from the param_name is an index but the parent is
+            not a list.
+        """
         if param_name.find(self._separator) > 0:
             pieces = param_name.split(self._separator)
 
@@ -149,12 +164,25 @@ class Dictionary:
                     dictionary = {param_name: value}
 
     def set(self, param_name: str, value: any = None):
+        """
+        Sets the given value into the given param_name path.
+
+        If the final key's parent does not exist, it sets it new
+        If the final key's parent exists and is a dict, it adds a new key with the value
+        If the final key's parent exists and is not a dict, overwrites it with a dict
+            consisting of the new key with the value
+
+        Raises a RuntimeError if any of the keys in the param_name does not exist
+        Raises a ValueError if a key from the param_name is an index but the parent is
+            not a list.
+        """
         if param_name is None:
             raise RuntimeError("Params must have a name")
 
         self.__recursive_set(param_name=param_name, value=value, dictionary=self._content)
 
     def key_exists(self, param_name: str) -> bool:
+        """Checks if the given param_name path exists, including the indexes inside the list ranges"""
         key_to_search = self._get_focused_key(param_name=param_name)
         parent_object = self.get_parent(param_name)
 
@@ -173,10 +201,12 @@ class Dictionary:
         return False
 
     def _get_focused_key(self, param_name: str) -> str:
+        """Returns the last key of the param_name"""
         return param_name.split(self._separator)[-1]\
             if param_name.find(self._separator) > 0 else param_name
 
     def get_parent(self, param_name: str) -> dict:
+        """Returns the parent object of the given param_name path"""
         if param_name.find(self._separator) > 0:
             pieces = param_name.split(self._separator)
             parent_key = self._separator.join(pieces[:-1])
@@ -185,7 +215,7 @@ class Dictionary:
             return self._content
 
     def delete(self, param_name: str) -> None:
-
+        """Deletes the given param_name path key"""
         if self.key_exists(param_name=param_name):
             parent = self.get_parent(param_name=param_name)
             key_to_delete = self._get_focused_key(param_name=param_name)
@@ -199,6 +229,12 @@ class Dictionary:
             return False
 
     def initialise_recursive(self, param_name: str) -> None:
+        """
+        Walks through the given param_name path and creates all missing keys and dicts.
+
+        Raises RuntimeError if a key of the param_name path already exists and it's not
+            a dictionary or a list (whatever expected), to avoid overwriting.
+        """
         pieces = param_name.split(self._separator)
         parent_path = ""
         # We start assuming that self._content is already {}
@@ -222,7 +258,7 @@ class Dictionary:
             parent_path = f"{path}{self._separator}"
 
     def get_keys_in(self, param_name: str = None) -> list:
-
+        """Returns the keys on the given param_name path dict"""
         if param_name is not None:
             obj = self.get(param_name=param_name)
         else:
@@ -236,4 +272,12 @@ class Dictionary:
             return None
 
     def to_dict(self) -> dict:
-        return self._content
+        """Shortcut to get_all()"""
+        return self.get_all()
+    
+
+    # @staticmethod
+    # def merge(origin: Dictionary, destination_path: str = None) -> None:
+    #     """
+    #     Takes 
+    #     """
