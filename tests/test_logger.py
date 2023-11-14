923db51091faf16a3a7cfd2e3978a475ceee1357
Xavi <xavi@arnaus.net>
2023-11-14 11:38:38 +0100

    Change the way Config is patched in Logger test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -4,12 +4,14 @@ from pyxavi.logger import Logger
 import logging
 
 CONFIG = {
-    "logger.format": "[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s",
-    "logger.filename": "test.log",
-    "logger.to_file": False,
-    "logger.to_stdout": True,
-    "logger.loglevel": 45,
-    "logger.name": "logger_test"
+    "logger": {
+        "format": "[%(asctime)s] %(levelname)-8s %(name)-12s %(message)s",
+        "filename": "test.log",
+        "to_file": False,
+        "to_stdout": True,
+        "loglevel": 45,
+        "name": "logger_test"
+    }
 }
 
 
@@ -20,15 +22,17 @@ def patched_config_init(self):
 def patched_config_get(self, param: str, default) -> str:
     return CONFIG[param] if param in CONFIG else default
 
+def patch_config_read_file(self):
+    self._content = CONFIG
 
-@patch("pyxavi.config.Config.__init__", new=patched_config_init)
-@patch("pyxavi.config.Config.get", new=patched_config_get)
+
+@patch.object(Config, "read_file", new=patch_config_read_file)
 def test_initialize_logger():
 
     config = Config()
 
     new_instance = Logger(config).get_logger()
 
-    from_logging = logging.getLogger(CONFIG["logger.name"])
+    from_logging = logging.getLogger(CONFIG["logger"]["name"])
 
     assert new_instance == from_logging
